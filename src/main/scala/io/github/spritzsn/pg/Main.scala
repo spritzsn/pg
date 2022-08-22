5316f67bca21bdfe2db2cad74897726fe8120be0
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-21 21:33:59 -0400

    update

--- src/main/scala/io/github/spritzsn/pg/Main.scala
+++ src/main/scala/io/github/spritzsn/pg/Main.scala
@@ -1,13 +1,13 @@
-package io.github.spritzsn.pg
-
-import io.github.spritzsn.async.*
-import io.github.spritzsn.libuv.defaultLoop
-
-import scala.util.{Failure, Success}
-
-@main def run(): Unit =
-  query("select * from cars") onComplete {
-    case Success(value)     => println(value)
-    case Failure(exception) => println(exception.getMessage)
-  }
-  defaultLoop.run()
+//package io.github.spritzsn.pg
+//
+//import io.github.spritzsn.async.*
+//import io.github.spritzsn.libuv.defaultLoop
+//
+//import scala.util.{Failure, Success}
+//
+//@main def run(): Unit =
+//  query("select * from cars") onComplete {
+//    case Success(value)     => println(value)
+//    case Failure(exception) => println(exception.getMessage)
+//  }
+//  defaultLoop.run()
