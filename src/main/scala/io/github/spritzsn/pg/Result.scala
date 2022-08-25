bba03cf33d8f023c5b3663eb365b764d28f206ab
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-25 17:49:51 -0400

    fix Result

--- src/main/scala/io/github/spritzsn/pg/Result.scala
+++ src/main/scala/io/github/spritzsn/pg/Result.scala
@@ -6,20 +6,28 @@ import io.github.edadma.table.TextTable
 import scala.collection.immutable.ArraySeq
 import scala.language.dynamics
 
-class Result(val columns: ArraySeq[String], val types: ArraySeq[Oid], val data: ArraySeq[ArraySeq[Any]]):
+class Result(val columns: ArraySeq[String], val types: ArraySeq[Oid], val data: ArraySeq[ArraySeq[Any]])
+    extends Dynamic:
   private val columnMap = columns.zipWithIndex.toMap
 
-  class Value private[Result] (v: Any):
+  class Value private[Result] (name: String, row: Int):
+    private val v =
+      columnMap get name match
+        case Some(col) => data(row)(col)
+        case None      => sys.error(s"field '$name' not found")
+
     def toInt: Int = v.asInstanceOf[Int]
+
     def toDouble: Double = v.asInstanceOf[Double]
 
     override def toString: String = v.toString
 
   class Row private[Result] (idx: Int) extends Dynamic:
-    def selectDynamic(field: String): Value =
-      columnMap get field match
-        case Some(col) => new Value(data(idx)(col))
-        case None      => sys.error(s"field '$field' not found")
+    def selectDynamic(field: String): Value = new Value(field, idx)
+
+  def selectDynamic(field: String): Value =
+    require(data.length == 1, s"result must contain exactly one row: ${data.length} row(s)")
+    new Value(field, 0)
 
   def apply(idx: Int): Row =
     require(0 <= idx && idx < data.length, s"row index out of range: $idx")
